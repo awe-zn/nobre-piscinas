06442209f349568ee8306000631b4ef4132b773c
Z4doque <t.zadoque@escolar.ifrn.edu.br>
2021-11-24 10:04:02 -0300

    finalizado o main e a section 'piscinas' com o carrossel

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,22 @@
-//index.js
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  slidesPerView: 1.25,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+    992: {
+      slidesPerView: 4
+    }
+  }
+});
