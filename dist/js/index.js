1a10cc6364e51301c93a04221bd84040f886c746
Z4doque <t.zadoque@escolar.ifrn.edu.br>
2022-01-27 13:49:39 -0300

    fixando o menu e adicionando carrossel na seção 'main'

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,19 @@
+//carrossel main
+const swiperMain = new Swiper('.swiper-main', {
+  slidesPerView: 1,
+  simulateTouch: false,
+  allowTouchMove: false,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 5000,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-main-next',
+    prevEl: '.swiper-main-prev',
+  },
+});
+
 //carrossel piscinas
 const swiper = new Swiper('.swiper', {
   pagination: {
